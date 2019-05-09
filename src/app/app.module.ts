48ef4031366b393e8e39af4f88e67f311b21932c
WilliamHXu <william.haofei.xu@gmail.com>
2019-05-09 11:07:52 -0400

    tar fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderbarComponent } from './headerbar/headerbar.component';
+import { GameComponent } from './game/game.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderbarComponent,
+    GameComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
